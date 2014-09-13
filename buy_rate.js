050473f160b48b047ed8a4b438c27f0f56f8fe21
webmake <webmake@users.noreply.github.com>
2014-09-13 13:45:50 +0300

    Create buy_rate.js

---
+++ buy_rate.js
@@ -0,0 +1,29 @@
+// buy
+type = 'buy';
+percent = 0.0025;
+if ('buy' == type) {
+    percent += 1;
+} else {
+    percent = 1 - percent;
+}
+amount = 5078.71;
+reach = '0.00244388';
+price = 0.00000048;
+counter = 0;
+status = 0;
+while (1) {
+    oldamount = amount;
+    amount += 0.00000001 * counter;
+    totalwithfee = price * amount * percent;
+    if (reach < totalwithfee.toFixed(8)) {
+        amount = oldamount;
+        status = 1;
+        if (counter == 1) {
+            console.log(amount);
+            break;
+        }
+        counter = 1;
+    } else {
+        counter++;
+    }
+}
